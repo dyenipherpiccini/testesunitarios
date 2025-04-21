3afecd06b33268295dcb9062667574d9a8d4c33a
Dyenipher Sthefany Batista Piccini <d.batista@intranet.maxiconsystems.com.br>
2025-04-21 16:27:19 -0300

    teste unitario 6

--- untitled/src/main/java/exercicio_06.java
+++ untitled/src/main/java/exercicio_06.java
@@ -1,12 +1,12 @@
 public class exercicio_06 {
-    public double calcularINSS(double salario) {
+
+    public Double calcularINSS(double salario) {
 
         double inss = 0;
-
         if (salario <= 1320.00) {
             inss = salario * 0.075;
         }
-        if (salario >= 1320.00 && salario <= 2571.29) {
+        if ((salario >= 1320.01) && (salario <= 2571.29)) {
             inss = salario * 0.09;
         }
         if (salario >= 2571.30 && salario <= 3856.94) {
@@ -15,6 +15,9 @@ public class exercicio_06 {
         if (salario >= 3856.95 && salario <= 7507.49) {
             inss = salario * 0.14;
         }
+        if (salario > 7507.49)
+            inss =  1051.04;
+
         return inss;
     }
 }
--- untitled/src/test/java/test_ex06.java
+++ untitled/src/test/java/test_ex06.java
@@ -1,2 +1,35 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class test_ex06 {
+
+    exercicio_06 exercicio_06;
+
+    @Before
+    public void before(){
+        exercicio_06 = new exercicio_06();
+    }
+
+    @Test
+    public void faixaUm () {
+        Assert.assertEquals("99.0", exercicio_06.calcularINSS(1320.00).toString());
+    }
+    @Test
+    public void faixaDois () {
+        Assert.assertEquals("118.89", exercicio_06.calcularINSS(1321.00).toString());
+    }
+    @Test
+    public void faixaTres () {
+        Assert.assertEquals("308.676", exercicio_06.calcularINSS(2572.30).toString());
+    }
+    @Test
+    public void faixaQuatro () {
+        Assert.assertEquals("1051.04", exercicio_06.calcularINSS(7510.49).toString());
+    }
+    @Test
+    public void faixaCinco () {
+        Assert.assertEquals("1051.04", exercicio_06.calcularINSS(7510.49).toString());
+    }
+
 }
